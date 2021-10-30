a3b47b460923c1c7346f66aadf9d9b887c640cc5
emarfer <sinatxe@LAPTOP-BIJ0FIE2.localdomain>
2021-10-30 22:25:52 +0200

    limpiando

--- misfunciones.py
+++ misfunciones.py
@@ -8,8 +8,14 @@ def entero_if (a):
     """
     a esta función se le pasa un float o un string, se le compara si es numerico o no y si lo es devuelve se le pasa a int.
     """
-    if a.isnumeric():
+    if a.isdigit():
         return int(a)
     else:
         return a
 
+def cambia (a):
+    """
+    recibe una lista y un número
+    cada elemento de la lista lo cambia por el número recibido
+    """
+    return a
