a9bc7303bee786a95e2eff2ef578618f3accc1a1
emarfer <sinatxe@LAPTOP-BIJ0FIE2.localdomain>
2021-11-01 16:03:39 +0100

    ahora sí está limpio

--- src/cleanfun.py
+++ src/cleanfun.py
@@ -1,3 +1,5 @@
+import re
+import pandas as pd
 def entero (a):
     """
     a esta función se le pasa un float o un string numérico y devuleve un entero.
@@ -21,3 +23,56 @@ def cambia (a):
     cada elemento de la lista lo cambia por el número recibido
     """
     return a
+
+
+def edades (col):
+    """
+    recibe un string
+    si contiene "months" devuelve el string tal cual
+    si contiene un número de uno o dos dígitos devuelve el número
+    si no contiene números devuelve el string en función del contenido del primero
+    """
+    if re.findall(".*months",col):
+        return col
+    elif re.findall("\d{1,2}",col):
+        return re.findall("\d{1,2}",col)[0]
+    elif re.findall(".*oung",col):
+        return "25"
+    elif re.findall(".*dul",col):
+        return "35"
+    elif re.findall(".*teen",col):
+        return "15"
+    elif re.findall(".*lder",col):
+        return "75"
+    elif re.findall(".*iddle",col):
+        return "50"
+    else:
+        return "UNKNOWN"
+
+def actividades (col):
+    """
+    recibe un string 
+    devuelve strings en función del contenido del recibido
+    """
+    if re.findall(".*[Uu](NKNOW|nknow).*",col):
+        return "Unknown"                  
+    elif re.findall(".*o injury.*",col):
+        return "No injury"
+    elif re.findall(".*[Ff](ATAL|atal).*",col):
+        return "Fatal injury"
+    elif re.findall(".*[Bb](itten|ITTEN).*",col):
+        return "Bitten"
+    elif re.findall(".*[Ll](acerat|ACERAT).*",col):
+        return "Lacerations"
+    elif re.findall(".*[Mm](inor|INOR).*",col):
+        return "Minor injuries"
+    elif re.findall(".*[Ss](EVERED|evered).*",col):
+        return "Limb severed"
+    elif re.findall(".*[Pp](unctur|UNCTUR).*",col):
+        return "Punctured"
+    elif re.findall(".*[Ii](njur|NJUR).*",col):
+        return "Injured"                  
+    elif re.findall(".*o detai.*",col):
+        return "No details"
+    else:
+        return "Others"
